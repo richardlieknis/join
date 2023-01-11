19bd5bb277f9e9c1b773078a3c60ced18eecf910
Shawn Kastner <Shawnkastner@hotmail.de>
2023-01-11 16:24:28 +0100

    create forgot password div

---
+++ js/forgotPassword.js
@@ -0,0 +1,22 @@
+function forgotPassword () {
+    document.getElementById('login-page').innerHTML = '';
+    document.getElementById('login-page').style = 'width: 826px; height: 462px'
+    document.getElementById('login-page').innerHTML += forgotPasswordTemplate();
+}
+
+function forgotPasswordTemplate() {
+    return /*html*/`
+    <div class="border-bottom display-center">
+        <img onclick="backToLogin()" class="position-arrow" src="../src/img/blueBackArrow.svg">
+        <div>
+            <h1 style="width: 626px">I forgot my password</h1>
+        </div>
+    </div>
+        <span style="text-align: center">
+            <b>Don't worry! We will send you an email with the instructions to <br>reset your password.</b>
+        </span>
+        <input class="input-login background-image-email" placeholder="Email" type="email" src="src/img/email.svg">
+        <div class="continue-btn-container">
+            <button style="width: 264px;" class="login-btn">Send me the email</button>
+    </div>`;
+}
