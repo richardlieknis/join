6e60ef4f0b0ab5950a4ccf1982abca3771b593f8
Marc Walter <m.walter-dev@gmx.de>
2023-01-12 18:41:46 +0100

    add highlighting for contacts

--- js/contacts.js
+++ js/contacts.js
@@ -1,1 +1,24 @@
 "use strict";
+
+/**
+ * This function is used to hightlight a contact at the contact list by clicking on it.
+ * 
+ * @param {number} contactId - This is the ID of the contact
+ */
+function highlightContact(contactId) {
+    let div = document.getElementById(`c-${contactId}`);
+    let initial = document.getElementById(`c-i-${contactId}`);
+    let name = document.getElementById(`c-name-${contactId}`);
+
+    div.classList.add('c-contact-overview-2');
+    div.classList.remove('c-contact-overview');
+    initial.classList.add('c-i-small-border');
+    name.classList.add('c-list-name2');
+}
+
+function removeHightlightContact() {
+    // array.forEach(element => {
+        
+    // });
+    // let allContactIds =
+}
