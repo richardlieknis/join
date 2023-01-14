be273fc5feb32de4f5014f34b7bf4922cec75414
Fabian Gretsch <91975420+fab0802@users.noreply.github.com>
2023-01-14 18:10:26 +0100

    added task overlay functionality

--- js/board.js
+++ js/board.js
@@ -5,6 +5,7 @@ let currentTasksArray = tasks;
 let filteredTasks = [];
 let currentDraggedElement;
 let categoryOfDraggedElement;
+const taskOverlayBg = document.querySelector("#taskOverlayBg");
 
 const dummyData = [
   {
@@ -12,10 +13,8 @@ const dummyData = [
     label: "design",
     title: "task a",
     description: "Modify the contents of the main website...",
-    subtasks: [
-      { title: "subtask 1", done: true },
-      { title: "subtask 2", done: false },
-    ],
+    dueDate: "05-08-2022",
+    subtasks: [],
     assignedTo: [
       {
         name: "Sandra Müller",
@@ -29,7 +28,7 @@ const dummyData = [
         email: "manu.vogel@gmail.com",
         phone: "+49934798347",
         color: "pink",
-        initials: "SM",
+        initials: "MV",
       },
     ],
     priority: "low",
@@ -40,6 +39,7 @@ const dummyData = [
     label: "sales",
     title: "task b",
     description: "Make the product presentation to prospective buyers",
+    dueDate: "05-08-2022",
     subtasks: [],
     assignedTo: [
       {
@@ -47,17 +47,31 @@ const dummyData = [
         email: "anton.so@gmx.net",
         phone: "+4998747394",
         color: "dark-blue",
-        initials: "SM",
+        initials: "AS",
       },
       {
         name: "Denise Eibold",
         email: "denise.e@gmail.com",
         phone: "+494556657",
         color: "red",
+        initials: "DE",
+      },
+      {
+        name: "Sandra Müller",
+        email: "sandra.mueller@gmx.de",
+        phone: "+4994937394",
+        color: "blue",
         initials: "SM",
       },
+      {
+        name: "Manuel Vogel",
+        email: "manu.vogel@gmail.com",
+        phone: "+49934798347",
+        color: "pink",
+        initials: "MV",
+      },
     ],
-    priority: "urgent",
+    priority: "medium",
     category: "todo",
   },
   {
@@ -65,6 +79,7 @@ const dummyData = [
     label: "backoffice",
     title: "task c",
     description: "Modify the contents of the main website...",
+    dueDate: "05-08-2022",
     subtasks: [
       { title: "subtask 1", done: true },
       { title: "subtask 2", done: false },
@@ -82,7 +97,7 @@ const dummyData = [
         email: "manu.vogel@gmail.com",
         phone: "+49934798347",
         color: "pink",
-        initials: "SM",
+        initials: "MV",
       },
     ],
     priority: "low",
@@ -93,6 +108,7 @@ const dummyData = [
     label: "media",
     title: "task d",
     description: "Make the product presentation to prospective buyers",
+    dueDate: "05-08-2022",
     subtasks: [],
     assignedTo: [
       {
@@ -100,14 +116,14 @@ const dummyData = [
         email: "anton.so@gmx.net",
         phone: "+4998747394",
         color: "dark-blue",
-        initials: "SM",
+        initials: "AS",
       },
       {
         name: "Denise Eibold",
         email: "denise.e@gmail.com",
         phone: "+494556657",
         color: "red",
-        initials: "SM",
+        initials: "DE",
       },
     ],
     priority: "urgent",
@@ -118,8 +134,9 @@ const dummyData = [
     label: "marketing",
     title: "task e",
     description: "Modify the contents of the main website...",
+    dueDate: "05-08-2022",
     subtasks: [
-      { title: "subtask 1", done: true },
+      { title: "subtask 1", done: false },
       { title: "subtask 2", done: false },
     ],
     assignedTo: [
@@ -135,7 +152,7 @@ const dummyData = [
         email: "manu.vogel@gmail.com",
         phone: "+49934798347",
         color: "pink",
-        initials: "SM",
+        initials: "MV",
       },
     ],
     priority: "low",
@@ -146,6 +163,7 @@ const dummyData = [
     label: "backoffice",
     title: "task f",
     description: "Make the product presentation to prospective buyers",
+    dueDate: "05-08-2022",
     subtasks: [],
     assignedTo: [
       {
@@ -153,14 +171,14 @@ const dummyData = [
         email: "anton.so@gmx.net",
         phone: "+4998747394",
         color: "dark-blue",
-        initials: "SM",
+        initials: "AS",
       },
       {
         name: "Denise Eibold",
         email: "denise.e@gmail.com",
         phone: "+494556657",
         color: "red",
-        initials: "SM",
+        initials: "DE",
       },
     ],
     priority: "urgent",
@@ -171,9 +189,10 @@ const dummyData = [
     label: "marketing",
     title: "task g",
     description: "Modify the contents of the main website...",
+    dueDate: "05-08-2022",
     subtasks: [
       { title: "subtask 1", done: true },
-      { title: "subtask 2", done: false },
+      { title: "subtask 2", done: true },
     ],
     assignedTo: [
       {
@@ -199,6 +218,7 @@ const dummyData = [
     label: "media",
     title: "task h",
     description: "Make the product presentation to prospective buyers",
+    dueDate: "05-08-2022",
     subtasks: [],
     assignedTo: [
       {
@@ -206,14 +226,14 @@ const dummyData = [
         email: "anton.so@gmx.net",
         phone: "+4998747394",
         color: "dark-blue",
-        initials: "SM",
+        initials: "AS",
       },
       {
         name: "Denise Eibold",
         email: "denise.e@gmail.com",
         phone: "+494556657",
         color: "red",
-        initials: "SM",
+        initials: "DE",
       },
     ],
     priority: "urgent",
@@ -241,7 +261,7 @@ function updateHtml(category, tasksArrayToRender) {
 // prettier-ignore
 function generateTaskCardtHtml(task) {
   return `
-        <div class="task-card" draggable="true" ondragstart="startDragging(${task.id})">
+        <div class="task-card" draggable="true" ondragstart="startDragging(${task.id})" onclick="openTaskOverlay(${task.id})">
             <span class="label ${task.label}">${task.label}</span>
             <div class="title">${task.title}</div>
             <div class="description">
@@ -255,9 +275,7 @@ function generateTaskCardtHtml(task) {
                 <div class="assigned-to">
                   ${getAssignedPersonsInitialsHtml(task)}
                 </div>
-                <div class="priority"><img src="../src/img/${
-                  task.priority
-                }.svg" /></div>
+                <div class="priority"><img src="../src/img/${task.priority}.svg" /></div>
             </div>
         </div>
   `;
@@ -327,4 +345,113 @@ function addPlaceholder() {
   }
 }
 
+function openTaskOverlay(taskId) {
+  renderLabel(taskId);
+  renderContentOnly(taskId, "title");
+  renderContentOnly(taskId, "description");
+  renderContentOnly(taskId, "dueDate");
+  renderSubtasksContainer(taskId);
+  renderPriority(taskId);
+  renderAssignedToContainer(taskId);
+
+  taskOverlayBg.classList.remove("d-none");
+}
+
+function closeTaskOverlay() {
+  taskOverlayBg.classList.add("d-none");
+}
+
 render();
+
+function renderLabel(taskId) {
+  const labelContainer = document.querySelector("#label");
+  labelContainer.className = "";
+  labelContainer.classList.add("label");
+  labelContainer.classList.add(tasks[taskId].label);
+  labelContainer.innerHTML = tasks[taskId].label;
+}
+
+function renderContentOnly(taskId, containerId) {
+  const container = document.querySelector(`#${containerId}`);
+  container.innerHTML = "";
+  container.innerHTML = tasks[taskId][containerId];
+}
+
+function renderSubtasksContainer(taskId) {
+  const subtaskContainer = document.querySelector("#subtasksContainer");
+  const subtaskElementsContainer = document.querySelector("#subtasks");
+  subtaskElementsContainer.innerHTML = "";
+  if (tasks[taskId].subtasks.length) {
+    subtaskElementsContainer.innerHTML = renderAllSubtasks(taskId);
+    subtaskContainer.className = "subtasks";
+  } else {
+    subtaskContainer.className = "subtasks d-none";
+  }
+}
+
+function renderAllSubtasks(taskId) {
+  let subtasksHtml = "";
+  const subtasksArray = [...tasks[taskId].subtasks];
+  for (let i = 0; i < subtasksArray.length; i++) {
+    subtasksHtml += generateSubtaskHtml(subtasksArray, i);
+  }
+  return subtasksHtml;
+}
+
+function generateSubtaskHtml(subtasksArray, subtaskId) {
+  return `
+      <div class="subtask" id="subtask-${subtaskId}">
+        <input
+          class="subtask-checkbox"
+          type="checkbox"
+          ${checkIfDone(subtasksArray, subtaskId)}
+        />
+        <span>${subtasksArray[subtaskId].title}</span>
+      </div>
+    `;
+}
+
+function checkIfDone(subtasksArray, subtaskId) {
+  if (subtasksArray[subtaskId].done) return "checked";
+}
+
+function renderPriority(taskId) {
+  const priorityContainer = document.querySelector("#priority");
+  priorityContainer.className = "";
+  priorityContainer.classList.add("label");
+  priorityContainer.classList.add(tasks[taskId].priority);
+  priorityContainer.innerHTML = `<span>${tasks[taskId].priority}</span>`;
+  priorityContainer.innerHTML += `<img src="../src/img/${tasks[taskId].priority}-white.svg" />`;
+}
+
+function renderAssignedToContainer(taskId) {
+  const assignedToContainer = document.querySelector("#assignedToContainer");
+  const assignedPersonsContainer = document.querySelector(
+    "#assignedPersonsContainer"
+  );
+  assignedPersonsContainer.innerHTML = "";
+  if (tasks[taskId].assignedTo.length) {
+    assignedPersonsContainer.innerHTML = renderAllAssignedPersons(taskId);
+    assignedToContainer.className = "assigned-persons-container";
+  } else {
+    assignedToContainer.className = "assigned-persons-container d-none";
+  }
+}
+
+function renderAllAssignedPersons(taskId) {
+  let assignedPersonHtml = "";
+  const assignedPersonArray = [...tasks[taskId].assignedTo];
+  for (let i = 0; i < assignedPersonArray.length; i++) {
+    assignedPersonHtml += generateAssignedPersonsHtml(assignedPersonArray, i);
+  }
+  return assignedPersonHtml;
+}
+
+function generateAssignedPersonsHtml(assignedPersonArray, assignedPersonId) {
+  return `
+      <div class="assigned-person">
+        <span class="initials ${assignedPersonArray[assignedPersonId].color}">${assignedPersonArray[assignedPersonId].initials}</span>
+        <span class="full-name">${assignedPersonArray[assignedPersonId].name}</span>
+      </div>
+    `;
+}
