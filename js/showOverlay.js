b1e3d7a11b205987ee6bfcf5274d6267d48ae646
Richard Lieknis <richard.lieknis@web.de>
2023-01-14 14:46:25 +0100

    overlay anim added

--- js/showOverlay.js
+++ js/showOverlay.js
@@ -1,12 +1,29 @@
 function openAddTask() {
     let addTaskOverlay = document.getElementById("addTaskOverlay");
+    let taskModul = document.getElementById('addTaskModul');
+    taskModul.classList.add("slideIn");
     addTaskOverlay.classList.remove("d-none");
 }
 
 function closeAddTask() {
-    addTaskOverlay.classList.add("d-none");
+    let taskModul = document.getElementById('addTaskModul');
+    taskModul.classList.remove("slideIn");
+    taskModul.classList.add("slideOut");
+    setTimeout(addDisplayNone, 550);
+}
+
+function openAddContact() {
+    //TODO
+}
+
+function closeAddContact() {
+
 }
 
 function dontClose(e) {
     e.stopPropagation();
+}
+
+function addDisplayNone() {
+    addTaskOverlay.classList.add("d-none")
 }
