6c941e96795401655dbd4837c10759f829fc1736
Fabian Gretsch <91975420+fab0802@users.noreply.github.com>
2023-01-14 10:54:12 +0100

    bugfix

--- js/editTasks.js
+++ js/editTasks.js
@@ -1,11 +1,10 @@
 let idCounter = 0;
-let subtasks = [],
+let subtasks = [];
 
 function createTask() {
-    const titel = document.getElementById('task-input-title');
-    const description = document.getElementById('task-input-description');
-    const category = document.getElementById('task-input-category');
-    const assignedTo = document.getElementById('task-input-assignedTo');
-    const dueDate = document.getElementById('task-input-dueDate');
-    
+  const titel = document.getElementById("task-input-title");
+  const description = document.getElementById("task-input-description");
+  const category = document.getElementById("task-input-category");
+  const assignedTo = document.getElementById("task-input-assignedTo");
+  const dueDate = document.getElementById("task-input-dueDate");
 }
