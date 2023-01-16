f2392178e9455c1ae1a6bf939a766e69ba267750
Shawn Kastner <Shawnkastner@hotmail.de>
2023-01-16 15:45:43 +0100

    created forgot-password.html

--- js/forgotPassword.js
+++ js/forgotPassword.js
@@ -8,10 +8,11 @@ async function onSubmit(event) {
     event.preventDefault(); 
     let formData = new FormData(event.target); // Create a FormData based on Form Element in HTML
     let response = await action(formData);
-    if(response.ok)
-        alert('Email was send!');
-    else
-        alert('Email not send!')
+    if(response.ok) {
+        document.getElementById('login-page').innerHTML += /*html*/`<div>
+            <img src="../src/img/send-mail.svg">
+        </div>`;
+    }
 }
 
 function action(formData) {
@@ -34,8 +35,8 @@ function getEmailUrlParameters() {
     return email;
 }
 
-function sendData() {
-
+function resetPassword() {
+    
 }
 
 function forgotPasswordTemplate() {
@@ -52,7 +53,7 @@ function forgotPasswordTemplate() {
         <form onsubmit="onSubmit(event)">
             <input id="forgot-password" required class="input-login background-image-email" placeholder="Email" name="email" type="email" src="src/img/email.svg">
         <div class="continue-btn-container">
-            <button type="submit" value="Submit" style="width: 264px; height: 51px" class="btn-primary">Send me the email</button>
+            <button id="mail-btn" type="submit" value="Submit" style="width: 264px; height: 51px" class="btn-primary">Send me the email</button>
         </div>
         </form>`;
 }
