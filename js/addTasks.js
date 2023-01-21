e714440519525210afabc817559aeb02d1347a65
Shawn Kastner <Shawnkastner@hotmail.de>
2023-01-21 10:44:05 +0100

    added actual date at add tasks

--- js/addTasks.js
+++ js/addTasks.js
@@ -1,5 +1,25 @@
 "use strict"
 
+let today = new Date();
+
+      let dd = today.getDate();
+      let mm = today.getMonth()+1; //January is 0!
+      let yyyy = today.getFullYear();
+
+      if(dd<10) {
+          dd = '0'+dd
+      } 
+
+      if(mm<10) {
+          mm = '0'+mm
+      } 
+
+      // today = yyyy + '/' + mm + '/' + dd;
+       today = yyyy + '-' + mm + '-' + dd;
+
+      console.log(today);
+      document.getElementById('task-input-dueDate').value = today;
+
 function renderAddTask() {
     console.log('reder');
 }
