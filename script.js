fee7abd7657985a5cc766e1f0c5bc1c375fc172e
Shawn Kastner <Shawnkastner@hotmail.de>
2023-01-25 14:43:44 +0100

    added logout popup

--- script.js
+++ script.js
@@ -10,31 +10,31 @@ let contacts = [
         phone: "+4994937394",
         color: "blue",
         initials: "SM",
-      },
-      {
+    },
+    {
         id: 2,
         name: "Manuel Vogel",
         email: "manu.vogel@gmail.com",
         phone: "+49934798347",
         color: "pink",
         initials: "MV",
-      },
-      {
+    },
+    {
         id: 3,
         name: "Anton Sommer",
         email: "anton.so@gmx.net",
         phone: "+4998747394",
         color: "dark-blue",
         initials: "AS",
-      },
-      {
+    },
+    {
         id: 4,
         name: "Denise Eibold",
         email: "denise.e@gmail.com",
         phone: "+494556657",
         color: "red",
         initials: "DE",
-      },
+    },
 ];
 let categories = [
     {
@@ -64,7 +64,16 @@ function getIndexOfArray(array, id) {
     for (let i = 0; i < array.length; i++) {
 
         if (id == array[i].id) index = i;
-    
+
     }
     return index;
+}
+
+function logoutPopUp() {
+    let userMenu = document.getElementById('user-menu');
+    if (userMenu.classList.contains('d-none')) {
+        userMenu.classList.remove('d-none');
+    } else {
+        userMenu.classList.add('d-none');
+    }
 }
