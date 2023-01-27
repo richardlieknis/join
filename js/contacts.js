ad801afa17b703512ffe36c30265e3341e5acdb0
unknown <m.walter-dev@gmx.de>
2023-01-27 14:18:01 +0100

    deselecting contact in contact list

--- js/contacts.js
+++ js/contacts.js
@@ -1,14 +1,15 @@
 "use strict";
 
 let sortedContacts = [];
+let highlightedContact;
 
 /**
  * This function is used to hightlight a contact at the contact list by clicking on it.
  * 
  * @param {number} contactId - This is the ID of the contact
  */
-async function highlightContact(contactId) {
-    await renderContacts();
+function highlightContact(contactId) {
+    unHighlightContact(contactId);
     let div = document.getElementById(`c-${contactId}`);
     let initial = document.getElementById(`c-i-${contactId}`);
     let name = document.getElementById(`c-name-${contactId}`);
@@ -17,6 +18,21 @@ async function highlightContact(contactId) {
     div.classList.remove('c-contact-overview');
     initial.classList.add('c-i-small-border');
     name.classList.add('c-list-name2');
+    highlightedContact = contactId;
+}
+
+function unHighlightContact(contactId) {
+    if (highlightedContact) {
+        console.log(contactId);
+        let div = document.getElementById(`c-${highlightedContact}`);
+        let initial = document.getElementById(`c-i-${highlightedContact}`);
+        let name = document.getElementById(`c-name-${highlightedContact}`);
+
+        div.classList.remove('c-contact-overview-2');
+        div.classList.add('c-contact-overview');
+        initial.classList.remove('c-i-small-border');
+        name.classList.remove('c-list-name2');
+    }
 }
 
 function removeHightlightContact() {
@@ -35,17 +51,17 @@ async function renderContacts() {
 }
 
 function sortContacts() {
-    contacts.sort(function(a, b) {
+    contacts.sort(function (a, b) {
         return compareStrings(a.name, b.name);
-      })
+    })
 }
 
 function compareStrings(a, b) {
     a = a.toLowerCase();
     b = b.toLowerCase();
-  
+
     return (a < b) ? -1 : (a > b) ? 1 : 0;
-  }
+}
 
 function generateContactList() {
     let html = ``;
@@ -59,9 +75,9 @@ function generateContactList() {
     });
     return html;
 }
-  
+
 function showContactDetail() {
-    document.getElementById(`c-view-mobile`).style = 'display: unset';
+    // document.getElementById(`c-view-mobile`).style = 'display: unset';
 }
 
 function goBackToContacts() {
