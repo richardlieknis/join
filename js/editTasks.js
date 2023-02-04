f90367f7d91c6ebbf9e4a440424a2b5d32331087
Marc Walter <m.walter-dev@gmx.de>
2023-02-04 11:24:29 +0100

    add function to assigned task

--- js/editTasks.js
+++ js/editTasks.js
@@ -10,7 +10,7 @@ async function createTask(status) {
     const title = document.getElementById("task-input-title");
     const description = document.getElementById("task-input-description");
     const category = document.getElementById("task-input-category");
-    const assignedTo = document.getElementById("task-input-assignedTo");
+    const assignedTo = getAssignedPersons();
     const dueDate = document.getElementById("task-input-dueDate");
 
     await setTasksIdCounter();
@@ -60,6 +60,27 @@ function clearTasksInputFields(title, description, category, assignedTo, dueDate
     assignedTo.value = "";
     dueDate.value = "";
     choosePriority("none")
+}
+
+function getAssignedPersons() {
+    const availavlePersons = getAllContactIds();
+    let assignedTo = [];
+
+    availavlePersons.forEach(i => {
+        if (getElementById(i).checked) {
+            assignedTo.push(i);
+        }
+    });
+    return assignedTo;
+}
+
+function getAllContactIds() {
+    let availavlePersons = [];
+
+    contacts.forEach(i => {
+        availavlePersons.push(i.id);
+    });
+    return availavlePersons;
 }
 
 // Edit Tasks
