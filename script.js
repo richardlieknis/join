2353bfed0f03a2a70ddd65ea5c49fd7e23a81ecc
Shawn Kastner <Shawnkastner@hotmail.de>
2023-03-09 11:56:11 +0100

    change setUrl

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let tasks = [];
 let contacts = [];
 let categories = [];
 
-setURL('https://marc-walter.developerakademie.net/join/smallest_backend');
+setURL('https://gruppe-join-422.developerakademie.net/smallest_backend');
 
 function getIndexOfArray(array, id) {
     let index;
